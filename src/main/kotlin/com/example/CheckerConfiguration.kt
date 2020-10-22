bc779ec5921557aeb8caf6eea0eafa9e2cc524b2
qiuzhanghua <qiuzhanghua@icloud.com>
2020-10-22 09:59:05 +0800

    try git + ssh

--- src/main/kotlin/com/example/CheckerConfiguration.kt
+++ src/main/kotlin/com/example/CheckerConfiguration.kt
@@ -1,10 +1,49 @@
 package com.example
 
+import com.jcraft.jsch.JSch
 import io.quarkus.arc.config.ConfigProperties
+import org.eclipse.jgit.transport.JschConfigSessionFactory
+import org.eclipse.jgit.transport.SshSessionFactory
+import org.eclipse.jgit.util.FS
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import java.util.ArrayList
+import javax.inject.Inject
+import javax.json.JsonArray
+import javax.json.JsonObject
+
 @ConfigProperties(prefix = "wc")
 class CheckerConfiguration {
     var root: String = ""   // where to save repos
-    var key: String = ""    // SSH key
+    var key: String = "/Users/quick/.ssh/id_rsa"    // SSH key
     var passwd: String = ""   // SSH key password
-    lateinit var repos: List<String>  // List of repository
+
+    @Inject
+    @ConfigProperty(name = "repos")
+    var repos: JsonObject? = null
+
+    fun getAllRepos(): HashMap<String, List<String>> {
+        var ans = HashMap<String, List<String>>()
+        repos?.entries?.forEach { entry ->
+            val key = entry.key
+            val v = entry.value as JsonArray
+            var list = ArrayList<String>()
+            v.forEach {
+                list.add(it.toString())
+            }
+            ans[key] = list
+        }
+        return ans
+    }
+
+
+    fun getSshSessionFactory() : SshSessionFactory {
+        return object : JschConfigSessionFactory() {
+            override fun createDefaultJSch(fs: FS?): JSch {
+                var jsch = super.createDefaultJSch(fs)
+                jsch.addIdentity(key, "")
+                return jsch
+            }
+        }
+    }
+
 }
--- src/main/kotlin/com/example/GitBean.kt
+++ src/main/kotlin/com/example/GitBean.kt
@@ -3,6 +3,7 @@ package com.example
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.diff.DiffFormatter
 import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.transport.SshTransport
 import org.eclipse.jgit.treewalk.AbstractTreeIterator
 import org.eclipse.jgit.treewalk.CanonicalTreeParser
 import org.eclipse.jgit.treewalk.EmptyTreeIterator
@@ -28,80 +29,92 @@ class GitBean {
         if (!root.exists()) {
             root.mkdir()
         }
-        for (s in config.repos) {
-            val lastIndex = s.lastIndexOf("/")
-            var s2 = s.substring(lastIndex)
-            val x = s2.lastIndexOf(".git")
-            if (x != -1) {
-                s2 = s2.substring(1 until x)
+
+        println("+++++++++++++++")
+        println(config.getAllRepos());
+        println("+++++++++++++++")
+        config.getAllRepos().entries.forEach { it ->
+            val id = it.key
+            val v = it.value
+
+            for (s in v) {
+                val lastIndex = s.lastIndexOf("/")
+                var s2 = s.substring(lastIndex)
+                val x = s2.lastIndexOf(".git")
+                if (x != -1) {
+                    s2 = s2.substring(1 until x)
 //                println(s2)
-            }
-            val path = config.root + File.separator + s2
+                }
+                val path = config.root + File.separator + s2
 
-            if (!File(path).exists()) {             // clone
-                val git = Git.cloneRepository()
-                        .setURI(s)
-                        .setDirectory(File(config.root + File.separator + s2))
-                        .call()
-                git.close()
-            } else {  // pull
+                if (!File(path).exists()) {             // clone
+                    val git = Git.cloneRepository()
+                            .setURI(s)
+                            .setTransportConfigCallback {transport ->
+                                (transport as SshTransport).sshSessionFactory = config.getSshSessionFactory()
+                            }
+                            .setDirectory(File(config.root + File.separator + s2))
+                            .call()
+                    git.close()
+                } else {  // pull
+                    val git = Git.open(File(path))
+                    val pull = git.pull()
+                    val result = pull.call()
+                    println(result)
+                    git.close()
+                }
+
                 val git = Git.open(File(path))
-                val pull = git.pull()
-                val result = pull.call()
-                println(result)
-                git.close()
-            }
-
-            val git = Git.open(File(path))
-            // use the following instead to list commits on a specific branch
-            //ObjectId branchId = repository.resolve("HEAD");
-            //Iterable<RevCommit> commits = git.log().add(branchId).call();
-            val commits: Iterable<RevCommit> = git.log().all().call()
-            var count = 0
-            val diffFormatter = DiffFormatter(NullOutputStream.INSTANCE)
-            diffFormatter.setRepository(git.repository)
-            for (commit in commits) {
-                // commit.commitTime  // commit 时间
-                val head = commit.tree
-                val parents = commit.parents
-                git.repository.newObjectReader().use { reader ->
-                    val oldTreeIter: AbstractTreeIterator
-                    if (parents.isNotEmpty()) {
-                        oldTreeIter = CanonicalTreeParser()
-                        val oldHead = commit.parents[0].tree
-                        oldTreeIter.reset(reader, oldHead)
-                    } else {
-                        oldTreeIter = EmptyTreeIterator()
-                    }
-                    val newTreeIter = CanonicalTreeParser()
-                    newTreeIter.reset(reader, head)
-                    println("**************")
-                    println(head.id)
-                    // var config = DiffConfig::getRenameLimit
-                    Git(git.repository).use { git ->
+                // use the following instead to list commits on a specific branch
+                //ObjectId branchId = repository.resolve("HEAD");
+                //Iterable<RevCommit> commits = git.log().add(branchId).call();
+                val commits: Iterable<RevCommit> = git.log().all().call()
+                var count = 0
+                val diffFormatter = DiffFormatter(NullOutputStream.INSTANCE)
+                diffFormatter.setRepository(git.repository)
+                for (commit in commits) {
+                    // commit.commitTime  // commit 时间
+                    val head = commit.tree
+                    val parents = commit.parents
+                    git.repository.newObjectReader().use { reader ->
+                        val oldTreeIter: AbstractTreeIterator
+                        if (parents.isNotEmpty()) {
+                            oldTreeIter = CanonicalTreeParser()
+                            val oldHead = commit.parents[0].tree
+                            oldTreeIter.reset(reader, oldHead)
+                        } else {
+                            oldTreeIter = EmptyTreeIterator()
+                        }
+                        val newTreeIter = CanonicalTreeParser()
+                        newTreeIter.reset(reader, head)
+                        println("**************")
+                        println(head.id)
+                        // var config = DiffConfig::getRenameLimit
+                        Git(git.repository).use { git ->
 //                        val diffs = git.diff()
 //                                .setNewTree(newTreeIter)
 //                                .setOldTree(oldTreeIter)
 //                                // .setShowNameAndStatusOnly(true)
 //                                .call()
-                        val diffs = diffFormatter.scan(oldTreeIter, newTreeIter)
-                        for (entry in diffs) {
-                            println("Entry: " + entry + ", from: " + entry.oldId + ", to: " + entry.newId)
-                            println(entry.newPath)  // 文件或者路径名
-                            val loader = git.repository.open(entry.newId.toObjectId())
-                            println("${loader.size} bytes, ${entry.newMode}, ${loader.type} , ${loader.isLarge}")
-                            val fileHeader = diffFormatter.toFileHeader(entry)
-                            for (hunk in fileHeader.hunks) {
-                                println("$hunk")
-                                println(hunk.toEditList())  // 可以用来计算增加了多少行，如果列表为空或者是[0-0]，就是增加或者二进制
+                            val diffs = diffFormatter.scan(oldTreeIter, newTreeIter)
+                            for (entry in diffs) {
+                                println("Entry: " + entry + ", from: " + entry.oldId + ", to: " + entry.newId)
+                                println(entry.newPath)  // 文件或者路径名
+                                val loader = git.repository.open(entry.newId.toObjectId())
+                                println("${loader.size} bytes, ${entry.newMode}, ${loader.type} , ${loader.isLarge}")
+                                val fileHeader = diffFormatter.toFileHeader(entry)
+                                for (hunk in fileHeader.hunks) {
+                                    println("$hunk")
+                                    println(hunk.toEditList())  // 可以用来计算增加了多少行，如果列表为空或者是[0-0]，就是增加或者二进制
+                                }
                             }
                         }
-                    }
 
+                    }
+                    count++
                 }
-                count++
+                println(count)
             }
-            println(count)
         }
     }
 }
